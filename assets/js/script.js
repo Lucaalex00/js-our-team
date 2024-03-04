3675feee760521616ce4009d58dd9fbe318fcff6
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-03-04 14:39:24 +0100

    add Milestone 2 commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,13 +44,27 @@ const team = [
         image: 'barbara-ramos-graphic-designer.jpg',
     },
 ];
+
 //Keep values from any element of 'TEAM' object's array
 for (const key in team) {
     /* console.log(team[key]) */
-
+    //keep and stamp
     let nome = team[key].name;
     let ruolo = team[key].role;
     let foto = team[key].image;
     console.log(`il nome è ${nome}, il suo ruolo è ${ruolo} e la sua foto è ${foto}`)
+
+
+
+
+}
+//keep from dom the DIV ELEMENT .container
+const container = document.querySelector('.container')
+//transform that key in a string if NOT, and stamp in DOM
+for (let i = 0; i < team.length; i++) {
+    const member = team[i];
+    console.log(member.name)
+    container.innerHTML += `<h3>Nome: ${member.name}</h3>  <div>Ruolo: ${member.role}<div> <img>Foto: ${member.image}</img>`
+
 }
 
