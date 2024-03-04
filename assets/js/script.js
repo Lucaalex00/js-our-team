5877b2ff81665564365c43db604957e4d5e915d0
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-03-04 15:07:13 +0100

    add Bonus 2 commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,12 +60,28 @@ for (const key in team) {
 }
 //keep from dom the DIV ELEMENT .container
 const container = document.querySelector('.container')
+
+//PARAGRAPH
+const containerParagraph = `<p class="text-center text-danger"> lorem ipsum dolor allen ist</p>`
+container.insertAdjacentHTML('afterbegin', containerParagraph)
+
+//TITLE
+const containerTitle = `<h4 class="text-center">OUR TEAM</h4>`
+container.insertAdjacentHTML('afterbegin', containerTitle)
+
+
+
+//BONUS2
+const rowElement = document.querySelector('.row');
+
 //transform that key in a string if NOT, and stamp in DOM
 for (let i = 0; i < team.length; i++) {
     const member = team[i];
-    console.log(member.name)
-    //STAMP IN DOM 
-    container.innerHTML += `<img src='./assets/img/${member.image}'> </img> <h3>Nome: ${member.name}</h3>  <div>Ruolo: ${member.role}<div>`
+    /*  console.log(member.name) */
+    //STAMP IN DOM + //BONUS 1
+    const markup = `<div class="col-4 g-4"><div class="card"><img class="border border-5" src='./assets/img/${member.image}'> </img> <div class="bg-secondary-subtle"><h4>Nome: ${member.name}</h4>  <div>Ruolo: ${member.role}</div></div></div></div>`
+    rowElement.insertAdjacentHTML('beforeend', markup)
+
 
 
 
